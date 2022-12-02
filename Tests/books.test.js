f52fab550d962dfc3837d581b7034ed7b05441e1
mckennahpalmer <mkpalm2006@gmail.com>
2022-12-01 17:25:36 -0700

    Got Jest working, just need to make the tests work

--- Tests/books.test.js
+++ Tests/books.test.js
@@ -1,36 +1,77 @@
-const { it, default: test } = require('node:test')
+//const { it, default: test, beforeEach } = require('node:test')
 const booksController = require("../controllers/books");
+
+let req, res;
+
+beforeEach(() => {
+  req = {};
+  send = jest.fn(),
+  res = {
+    status: jest.fn(() => {
+      send
+    }),
+    json: jest.nf(),
+  }
+});
 
 describe("Books Tests", () => {
 
-  it('Call all books', () =>{
-    const result = mongodb.getDb().db().collection("books").find();
-    expect(booksController.getAllBooks(result)).toBe(res.status(200));
+  describe("Call all books", () => {
+    beforeEach(() => {
+      //might need this for authentication later.
+    });
+
+    it('Responds with a 200', () =>{
+      booksController.index(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    it("responds with a 401", () => {
+      booksController.index(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+    });
 
   });
 
-  it('Retrieve a book by the OId', () =>{
-    const Id = '637ee05926a634d0f54729f8';
-    const bookOne = [{
+  describe("Retrieve a book by the OId", () => {
+    it('responds with the json file of the book', () => {
+      const Id = '637ee05926a634d0f54729f8';
+      const bookOne = [{
         title: "To Kill a Mockingbird",
         author: "Harper Lee",
         yearPublished: "1960",
         format: "Paperback"
       }];
 
-    expect(booksController.getBookById(Id).toBe(bookOne));
+      expect(booksController.getBookById(Id).toBe(bookOne));
+    });
 
+    it("responds with the proper error message", () => {});
   });
 
-  it('Add a new book to the collection', () =>{
-    expect(booksController.getAllBooks(res.status(200))).toBe(json(lists));
+  describe("Add new book", () => {
+    it('Adds book and return successful code', () => {
+      expect(booksController.getAllBooks(res.status(200))).toBe(json(lists));
+    });
+
+    it("display the error message", () => {});
   });
 
-  it('Update the book', () =>{
-    expect(booksController.getAllBooks(res.status(200))).toBe(json(lists));
+  describe("Update the book", () => {
+    it('changes some info and saves it', () =>{
+      expect(booksController.getAllBooks(res.status(200))).toBe(json(lists)); 
+    });
+
+    it("display the error message", () => {});
   });
 
-  it('Delete the book', () =>{
-    expect(booksController.getAllBooks(res.status(200))).toBe(json(lists));
+  describe("Deletes the book", () => {
+    it('Removes the book we added earler from the database', () =>{
+      expect(booksController.getAllBooks(res.status(200))).toBe(json(lists));
+    });
+
+    it("display the error message", () => {});
   });
 });
